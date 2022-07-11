5926a70c3fce874aacc3f7f1965c6ad633d722b1
tetelevm <tetelev99@mail.ru>
2022-07-11 22:58:59 +0300

    Added frozendict to snippets

--- python_snippets.py
+++ python_snippets.py
@@ -5,7 +5,6 @@ separate file.
 TODO: it needs to be documenting
 """
 
-
 # ======= SINGLETON ====================================================
 
 
@@ -20,7 +19,6 @@ class SingletonMeta(type):
 
 class Singleton(metaclass=SingletonMeta):
     pass
-
 
 # ======= EXCEPTION FROM DOC ===========================================
 
@@ -45,22 +43,20 @@ class ExceptionFromFormattedDoc(ExceptionFromDoc):
             msg += '\n' + self.__doc__ + '\n' + str(formats)
         super(ExceptionFromFormattedDoc, self).__init__(msg)
 
-
 # ======== FOLDER WALK =================================================
 
 
-import os
+import os  # can be replaced by `pathlib`
 
 
 def get_all_files_from_folder(path):
     all_files = list()
     for (folder, _, files) in os.walk(path):
         for file in files:
-            all_files.append(f'{folder}/{file}')
+            all_files.append(os.path.join(folder, file))
     return all_files
 
-
-# ======== =============================================================
+# ======== INPUT FUNC ==================================================
 
 
 def do_input(description='', args=('y', 'n')):
@@ -72,22 +68,58 @@ def do_input(description='', args=('y', 'n')):
         answer = input(msg)
     return answer
 
-
-# ======== =============================================================
+# ======== FIND SUBCLASSES IN FILE =====================================
 
 
 def child_classes(locals_, parent_class):
-    def _is_subclass(cls):
+    def _is_subclass(name, cls):
         return (
-                isinstance(cls, type)  # is class
+                isinstance(cls, parent_class.__class__)  # is class
                 and issubclass(cls, parent_class)  # is subclass
                 and cls != parent_class  # not parent class
+                and name == cls.__name__  # not a link
         )
     return [
         name
         for (name, cls) in locals_.items()
-        if _is_subclass(cls)
+        if _is_subclass(name, cls)
     ]
 
+# ======== FROZENDICT ==================================================
 
-# ======== =============================================================
+
+class frozendict(dict):
+
+    @classmethod
+    def fromkeys(cls, iterable, value=None):
+        return cls(super().fromkeys(iterable, value))
+
+    def __setitem__(self, key, value):
+        raise NotImplementedError("Cannot add or change values")
+
+    def __delitem__(self, key):
+        raise NotImplementedError("Cannot add or change values")
+
+    def __repr__(self):
+        return "f" + super().__repr__()
+
+    def __ior__(self, other):
+        raise NotImplementedError("Cannot add or change values")
+
+    def setdefault(self, key="", default=None):
+        # Can be replaced by raising an NotImplementedError
+        return self.get(key, default)
+
+    def clear(self):
+        raise NotImplementedError("frozendict does not have clear method")
+
+    def pop(self, key):
+        raise NotImplementedError("frozendict does not have pop method")
+
+    def popitem(self):
+        raise NotImplementedError("frozendict does not have popitem method")
+
+    def update(self, m, **kwargs):
+        raise NotImplementedError("frozendict does not have update method")
+
+# ========  ============================================================
